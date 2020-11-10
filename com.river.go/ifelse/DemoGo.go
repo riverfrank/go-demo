28d3084433af876ce138f367e066456ae404e5f4
river <yybffds@126.com>
2020-11-10 10:17:42 +0800

    测试提交代码

--- com.river.go/ifelse/DemoGo.go
+++ com.river.go/ifelse/DemoGo.go
@@ -6,29 +6,31 @@ import (
 )
 
 func main() {
-	i:=strings.Index("飞雪无情","飞雪")
+	i := strings.Index("飞雪无情", "飞雪")
 	fmt.Println(i)
 
 	if i > 0 {
 		fmt.Println("hello")
-	}else {
+	} else {
 		fmt.Println("no")
 	}
 
-	switch j:=1 ;{
+	switch j := 1; {
 	case j == 1:
-			fmt.Println("1")
-			fallthrough
-	case j== 2:
+		fmt.Println("1")
+		fallthrough
+	case j == 2:
 		fmt.Println("2")
-	case j== 3:
+	case j == 3:
 		fmt.Println("2")
 	}
 
 	sum := 0
-	for i := 1 ; i <= 100 ; i++ {
+	for i := 1; i <= 100; i++ {
 		sum = sum + i
 	}
 	fmt.Println(sum)
+
+	fmt.Println("hello river")
 
 }
