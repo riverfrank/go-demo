18f86ef6726da780df736bbf2d3f5f83973c6ccc
river <yybffds@126.com>
2020-11-20 17:30:43 +0800

    添加测试

--- com.river.go/chan/ChanTest.go
+++ com.river.go/chan/ChanTest.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	ch := make(chan int)
+	/**
+	 * 测试结果
+	 * channel无缓冲时，发送阻塞直到数据被接收，接收阻塞直到读到数据。
+	 * channel有缓冲时，当缓冲满时发送阻塞，当缓冲空时接收阻塞。
+	 */
+	ch := make(chan int, 0)
 	go func() {
 		var sum int
 		for i := 0; i < 10; i++ {
@@ -11,14 +19,13 @@ func main() {
 		}
 		fmt.Println(sum)
 		ch <- sum
-
-		ch <- 5
-		ch <- 4
-		ch <- 3
-		ch <- 2
-		fmt.Println(2)
+		//ch <- 22
+		fmt.Println("--------->")
 
 	}()
+	time.Sleep(time.Second * 2)
 	fmt.Println("hello <- ch", <-ch)
-	fmt.Println("hello <- ch", <-ch)
+	//fmt.Println("hello <- ch", <-ch)
+	time.Sleep(time.Second * 1)
+
 }
